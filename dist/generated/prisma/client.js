50333b5977aeac28f15d29e15157781a83897a7d
pyae-bhone-khant <pyaebhonen4@gmail.com>
2026-05-08 23:17:06 +0630

    Remove binaryTargets from Prisma schema to fix client corruption

--- dist/generated/prisma/client.js
+++ dist/generated/prisma/client.js
@@ -71,7 +71,4 @@ exports.PrismaClient = $Class.getPrismaClientClass(__dirname);
 // file annotations for bundling tools to include these files
 path.join(__dirname, "query_engine-windows.dll.node");
 path.join(process.cwd(), "generated/prisma/query_engine-windows.dll.node");
-// file annotations for bundling tools to include these files
-path.join(__dirname, "libquery_engine-debian-openssl-3.0.x.so.node");
-path.join(process.cwd(), "generated/prisma/libquery_engine-debian-openssl-3.0.x.so.node");
 //# sourceMappingURL=client.js.map
